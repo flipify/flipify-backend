6aa997e1f50d8b3562fb5113c289ecc3ced4c002
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-06-23 21:46:09 +0100

    Rename the project package from CORE
    Renamed the project package from CORE to flipify_backend.
    Reason: CORE is also a python package so we should adhere
    to python's code guides.

--- flipify_backend/wsgi.py
+++ flipify_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CORE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flipify_backend.settings')
 
 application = get_wsgi_application()
