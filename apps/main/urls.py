5aa0b5feab89173372697a428f0e174218cb0c70
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-06-24 11:55:58 +0100

    Define Views
    Define views for `Platform` and `Technology` model and also
    added `api_root` view that provides links to all the endpoint
    available on flipify.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -9,7 +9,10 @@ from rest_framework_simplejwt.views import (
 
 # Note: Auth urls have been moved to its app.
 urlpatterns = [
+    path('', views.api_root),
     path('greet/', views.greet, name='greet'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('platforms/', views.PlatformView.as_view(), name='platform_list'),
+    path('technologies/', views.TechnologyView.as_view(), name='tech_list'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,9 +1,33 @@
 from rest_framework.decorators import api_view
+from rest_framework import permissions, generics
+from rest_framework.reverse import reverse
 from rest_framework.response import Response
+from apps.main import models, serializers
 
 
+@api_view(['GET'])
+def api_root(request):
+    # TODO: Remove this view in the future if we use DefaultRouter.
+    """
+    This view provides a list of
+    all the endpoints available
+    on flipify.
+    """
+    urls = {
+        'greet': reverse('greet', request=request),
+        'login': reverse('token_obtain_pair', request=request),
+        'login-refresh': reverse('token_refresh', request=request),
+        'github-login': reverse('github_login', request=request),
+        'gitlab-login': reverse('gitlab_login', request=request),
+        'github-connect': reverse('github_connect', request=request),
+        'gitlab-connect': reverse('gitlab_connect', request=request),
+        'platforms': reverse('platform_list', request=request),
+        'technologies': reverse('tech_list', request=request),
+    }
+    return Response(urls)
 
-@api_view(['GET',])
+
+@api_view(['GET', ])
 def greet(request):
     """
     Greets the user with a message.
@@ -13,3 +37,24 @@ def greet(request):
         return Response(status=405)
     name = request.query_params.get('name', 'World')
     return Response({'message': f'Hello, {name}!'})
+
+
+class PlatformView(generics.ListAPIView):
+    """
+    Returns a list of platforms a flipify user
+    can switch from or switch to and their current
+    status.
+    """
+    queryset = models.Platform.objects.all()
+    serializer_class = serializers.PlatformSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class TechnologyView(generics.ListAPIView):
+    """
+    Returns a list of technologies or frameworks
+    supported by flipify.
+    """
+    queryset = models.Technology.objects.all()
+    serializer_class = serializers.TechnologySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
