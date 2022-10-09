0461019388ced0db77afde4a7fad283683b1c371
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-10-09 19:21:35 +0100

    Fix issues with authentication views

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from apps.authentication.views import (
-    GitHubLogin,
-    GitLabLogin,
     GitHubConnect,
+    GitHubLogin,
     GitLabConnect,
+    GitLabLogin,
 )
 
+from dj_rest_auth.views import LogoutView
+
 urlpatterns = [
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("github/", GitHubLogin.as_view(), name="github_login"),
     path("gitlab/", GitLabLogin.as_view(), name="gitlab_login"),
     path("github/connect/", GitHubConnect.as_view(), name="github_connect"),
     path("gitlab/connect/", GitLabConnect.as_view(), name="gitlab_connect"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login-refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
