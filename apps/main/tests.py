ad54de62d5eeb11b04c8039bca8a7bcf0e6d33c3
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-10-09 19:19:23 +0100

    update tests

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -1,9 +1,13 @@
-from django.test import TestCase
+from re import A
+
+from django.conf import settings
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 
-class TestMain(TestCase):
-    def test_greet_view(self):
-        # TODO: Remove hardcoded url test.
-        response = self.client.get("/api/v1/greet/?name=Flipify")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {"message": "Hello, Flipify!"})
+class TestMain(APITestCase):
+    def test_greet_view_response_data_and_status_code(self):
+        response = self.client.get(reverse("greet") + "?name=Flipify")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertDictEqual(response.data, {"message": "Hello, Flipify!"})
